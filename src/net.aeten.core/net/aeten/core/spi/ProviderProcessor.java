c0035242ded35deb14879ecd831cfd036485e3e7
Thomas Pérennou <thomas.perennou@aeten.net>
2014-03-22 20:09:05 +0100

    Overrite generated file

--- src/net.aeten.core/net/aeten/core/spi/ProviderProcessor.java
+++ src/net.aeten.core/net/aeten/core/spi/ProviderProcessor.java
@@ -1,5 +1,7 @@
 package net.aeten.core.spi;
 
+import static javax.lang.model.SourceVersion.RELEASE_8;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -12,14 +14,20 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
 import javax.annotation.Generated;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
-import static javax.lang.model.SourceVersion.RELEASE_8;
-import javax.lang.model.element.*;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.AnnotationValue;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.TypeMirror;
 import javax.tools.FileObject;
 import javax.tools.StandardLocation;
@@ -163,7 +171,7 @@ public class ProviderProcessor extends AbstractProcessor {
 
 		try {
 			FileObject fileObject = processingEnv.getFiler().createSourceFile(pkg + "." + name, element);
-			try (PrintWriter writer = getWriter(fileObject, WriteMode.CREATE, false)) {
+			try (PrintWriter writer = getWriter(fileObject, WriteMode.OVERRIDE, false)) {
 				TypeMirror initializerType = null;
 				Iterator<? extends TypeMirror> thrownTypes = null;
 				for (Element enclosedElement: providerElement.getEnclosedElements()) {
